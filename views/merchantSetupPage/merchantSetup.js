e2ca639b6feae50043fb540ab0fd41b6186061df
Lee Morgan <Morgan.LeeR@protonmail.com>
2019-09-27 17:26:24 -0400

    Commit for experimental changes

--- views/merchantSetupPage/merchantSetup.js
+++ views/merchantSetupPage/merchantSetup.js
@@ -2,9 +2,16 @@ console.log(ingredients);
 
 //Populate all ingredients into the table
 let table = document.querySelector("#ingredient-display");
+let createDiv = document.querySelector("#new-ingredient");
+createDiv.style.display = "none";
+
+let existingIngredients = [];
+let newIngredients = [];
+let data = {};
 
 for(let ingredient of ingredients){
     let row = document.createElement("tr");
+    row.id = ingredient._id;
 
     let add = document.createElement("td");
     let checkbox = document.createElement("input");
@@ -33,5 +40,50 @@ for(let ingredient of ingredients){
     unit.innerText = ingredient.unitType;
     row.appendChild(unit);
 
+    let idField = document.createElement("input");
+    console.log(ingredient);
+    idField.type = "hidden";
+    idField.value = ingredient._id;
+
     table.appendChild(row);
+    existingIngredients.push(row);
+}
+
+//Gather all data from both forms and submit
+let subData = ()=>{
+    data.existing = [];
+    for(let ingredient of existingIngredients){
+        if(ingredient.childNodes[0].firstChild.checked){
+            data.existing.push(ingredient.id);
+        }
+    }
+
+    table.style.display = "none";
+    createDiv.style.display = "block";
+}
+
+//Add a field for inputing a new ingredient
+let addField = ()=>{
+    let ingredient = document.createElement("div");
+    ingredient.classList += "input-new";
+
+    let name = document.createElement("input");
+    name.type = "text";
+    ingredient.appendChild(name);
+
+    let category = document.createElement("input");
+    category.type = "text";
+    ingredient.appendChild(category);
+
+    let quantity = document.createElement("input");
+    quantity.type = "number";
+    quantity.step = "0.01";
+    ingredient.appendChild(quantity);
+
+    let unit = document.createElement("input");
+    unit.type = "text";
+    ingredient.appendChild(unit);
+
+    div.appendChild(ingredient);
+    newIngredients.push(ingredient);
 }
