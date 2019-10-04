3eb097ece60d107fc9abb1a701450e8c17a97092
Lee Morgan <Morgan.LeeR@protonmail.com>
2019-10-04 14:10:02 -0400

    Complete basic home page

--- views/inventory/inventory.js
+++ views/inventory/inventory.js
@@ -1,18 +1,7 @@
-console.log(merchant);
-
-let fullList = [];
-for(let item of merchant.inventory){
-    fullList.push({
-        name: item.ingredient.name,
-        category: item.ingredient.category,
-        quantity: item.quantity,
-        unit: item.ingredient.unitType 
-    });
-}
-
+let items = [];
 let tbody = document.querySelector("tbody");
 
-let renderIngredients = (items = fullList)=>{
+let renderIngredients = ()=>{
     while(tbody.hasChildNodes()){
         tbody.removeChild(tbody.firstChild);
     }
@@ -39,21 +28,27 @@ let renderIngredients = (items = fullList)=>{
     }
 }
 
-let sortIngredients = (items = fullList)=>{
-    items.sort((a, b) => (a.name > b.name) ? 1 : -1)
+let sortIngredients = (property)=>{
+    items.sort((a, b) => (a[property] > b[property]) ? 1 : -1);
     renderIngredients();
 }
 
 let filter = ()=>{
-    let items = [];
+    items = [];
     let searchString = document.querySelector("#filter").value;
-    for(let item of fullList){
-        if(item.name.toLowerCase().includes(searchString)){
-            items.push(item);
+    for(let item of merchant.inventory){
+        if(item.ingredient.name.toLowerCase().includes(searchString)){
+            items.push({
+                name: item.ingredient.name,
+                category: item.ingredient.category,
+                quantity: item.quantity,
+                unit: item.ingredient.unitType
+            });
         }
     }
 
+    sortIngredients("name");
     renderIngredients(items);
 }
 
-sortIngredients();
+filter();
