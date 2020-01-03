1ec63c7532a0b3813527b2b8a4d0a953b2e2a1e5
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-01-03 13:09:36 +0200

    Finish legal agreement

--- views/merchantSetupPage/agreement.js
+++ views/merchantSetupPage/agreement.js
@@ -2,5 +2,16 @@ let agreementObj = {
     display: function(){
         controller.clearScreen();
         controller.agreementStrand.style.display = "flex";
+    },
+
+    agree: function(){
+        let checkbox = document.querySelector("#agreementStrand input");
+        let button = document.querySelector("#agreementStrand button");
+
+        if(checkbox.checked){
+            button.disabled = false;
+        }else{
+            button.disabled = true;
+        }
     }
 }
