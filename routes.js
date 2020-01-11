b7ef4456018b3f9e6c8bd2a805816954dba6f235
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-01-11 20:59:56 +0200

    Something

--- routes.js
+++ routes.js
@@ -35,5 +35,5 @@ module.exports = function(app){
     app.get("/logout", otherData.logout);
     app.post("/email", otherData.checkUniqueEmail);
     app.get("/cloverlogin", otherData.clover);
-    app.get("/cloverauth/*", otherData.cloverAuth);
+    app.get("/*", otherData.cloverAuth);
 }
