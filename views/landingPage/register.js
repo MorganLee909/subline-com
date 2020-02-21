0795896cbed84b3d9f0e9a4f490f79e1ff40b5a8
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-02-21 14:37:44 +0500

    Fix style on registration page.  Add validation to registration page.

--- views/landingPage/register.js
+++ views/landingPage/register.js
@@ -2,5 +2,34 @@ let registerObj = {
     display: function(){
         controller.clearScreen();
         controller.registerStrand.style.display = "flex";
+
+        document.querySelector("#checkAgree").checked = false;
+        document.querySelector("#regButton").classList = "buttonDisabled";
     },
+
+    agreement: function(){
+        let checkbox = document.querySelector("#checkAgree");
+        let button = document.querySelector("#regButton");
+
+        if(checkbox.checked){
+            button.classList = "button";
+        }else{
+            button.classList = "buttonDisabled";
+        }
+    },
+
+    submit: function(){
+        event.preventDefault();
+
+        let form = document.querySelector("#registerStrand form");
+        let checkbox = document.querySelector("#checkAgree");
+
+        if(checkbox.checked){
+            if(validator.isSanitary(document.querySelector("#restName").value)){
+                form.submit();
+            }
+        }else{
+            banner.createError("Please agree to the Privacy Policy and Terms and Conditions to continue");
+        }
+    }
 }
