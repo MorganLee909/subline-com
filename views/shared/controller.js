950f243cbedce96d06e3eaf299101b3975b446ef
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-04-10 16:15:56 +0500

    Finish sidebar menu

--- views/shared/controller.js
+++ views/shared/controller.js
@@ -3,6 +3,15 @@ let changeStrand = (name)=>{
         strand.style.display = "none";
     }
 
+    for(let button of document.querySelectorAll(".menu > button")){
+        button.classList = "";
+        button.onclick = ()=>{changeStrand(`${button.id.slice(0, button.id.indexOf("Btn"))}Strand`)};
+    }
+
+    let activeButton = document.querySelector(`#${name.slice(0, name.indexOf("Strand"))}Btn`);
+    activeButton.classList = "active";
+    activeButton.onclick = undefined;
+
     document.querySelector(`#${name}`).style.display = "flex";
     window[`${name}Obj`].display();
 }
