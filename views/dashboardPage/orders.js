fc1e06f6f852ad91058032f4b0aa9ac174d77c33
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-05-24 15:11:36 +0300

    Add just the js for fetching and displaying orders

--- views/dashboardPage/orders.js
+++ views/dashboardPage/orders.js
@@ -14,7 +14,22 @@ window.ordersStrandObj = {
                     if(typeof(response) === "string"){
                         banner.createError(response);
                     }else{
-                        
+                        let tbody = document.querySelector("#orderList tbody");
+                        let template = document.querySelector("#order").content.children[0];
+
+                        for(let i = 0; i < response.length; i++){
+                            let row = template.cloneNode(true);
+                            let totalCost = 0;
+                            
+                            for(let j = 0; j < response[i].ingredients; j++){
+                                totalCost += response[i].ingredients[j].quantity * response[i].ingredients[j].price;
+                            }
+
+                            row.children[0].innerText = response[i]._id;
+                            row.children[1].innerText = response[i].ingredients.length;
+                            row.children[2].innerText = response[i].date;
+                            row.children[3].innerText = totalCost;
+                        }
                     }
                 })
                 .catch((err)=>{
