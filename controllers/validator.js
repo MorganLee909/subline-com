a23dc8e33faa3df020f0b6d1cbc762c74d8627ba
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-06-22 12:07:18 +0300

    Fix duplication of recipe ingredients when editing

--- controllers/validator.js
+++ controllers/validator.js
@@ -85,6 +85,14 @@ module.exports = {
             }
         }
 
+        for(let i = 0; i < recipe.ingredients.length; i++){
+            for(let j = i + 1; j < recipe.ingredients.length; j++){
+                if(recipe.ingredients[i].ingredient === recipe.ingredients[j].ingredient){
+                    return "Recipe cannot contain duplicate ingredients";
+                }
+            }
+        }
+
         return true;
     },
 
--- views/dashboardPage/components/components.js
+++ views/dashboardPage/components/components.js
@@ -81,7 +81,7 @@ let recipeDetailsComp = {
                 this.recipe.ingredients.push({
                     ingredient: select.options[select.selectedIndex].ingredient,
                     quantity: divs[i].children[1].value
-                })
+                });
             }else{
                 this.recipe.ingredients.push({
                     ingredient: divs[i].ingredient,
