c41f63aa44c3c61e6b0537387b3ebf880ff7da06
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-06-25 17:35:32 +0300

    Add a transaction details sidebar

--- views/dashboardPage/transactions.js
+++ views/dashboardPage/transactions.js
@@ -2,16 +2,13 @@ window.transactionsStrandObj = {
     isPopulated: false, 
 
     display: function(){
-        console.time("populate transactions");
         if(!this.isPopulated){
-            let now = new Date();
-            let firstOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
-
             let transactionsList = document.getElementById("transactionsList");
             let template = document.getElementById("transaction").content.children[0];
 
             for(let i = merchant.transactions.length - 1; i > merchant.transactions.length - 101 ; i--){
                 let transaction = template.cloneNode(true);
+                transaction.onclick = ()=>{transactionDetailsComp.display(merchant.transactions[i])};
                 transactionsList.appendChild(transaction);
 
                 transaction.children[0].innerText = `${merchant.transactions[i].date.toLocaleDateString()} ${merchant.transactions[i].date.toLocaleTimeString()}`;
@@ -20,6 +17,5 @@ window.transactionsStrandObj = {
 
             this.isPopulated = true;
         }
-        console.timeEnd("populate transactions");
     }
 }
