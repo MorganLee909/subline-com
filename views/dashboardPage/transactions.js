2977155656db1f1ffa8be212d2330c1bf33a3f24
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-06-25 12:21:39 +0300

    Populate and display transactions

--- views/dashboardPage/transactions.js
+++ views/dashboardPage/transactions.js
@@ -2,21 +2,24 @@ window.transactionsStrandObj = {
     isPopulated: false, 
 
     display: function(){
+        console.time("populate transactions");
         if(!this.isPopulated){
             let now = new Date();
             let firstOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
 
-            let dates = merchant.transactionIndices(firstOfMonth);
             let transactionsList = document.getElementById("transactionsList");
             let template = document.getElementById("transaction").content.children[0];
 
-            for(let i = dates[0]; i < dates[1]; i++){
+            for(let i = merchant.transactions.length - 1; i > merchant.transactions.length - 101 ; i--){
                 let transaction = template.cloneNode(true);
                 transactionsList.appendChild(transaction);
 
                 transaction.children[0].innerText = `${merchant.transactions[i].date.toLocaleDateString()} ${merchant.transactions[i].date.toLocaleTimeString()}`;
                 transaction.children[1].innerText = `${merchant.transactions[i].recipes.length} recipes sold`;
             }
+
+            this.isPopulated = true;
         }
+        console.timeEnd("populate transactions");
     }
 }
