51e85781b444dcaa244a149b0db2c5b5ac6b82e7
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-06-27 09:43:50 +0300

    Update transactions display

--- views/dashboardPage/transactions.js
+++ views/dashboardPage/transactions.js
@@ -15,8 +15,17 @@ window.transactionsStrandObj = {
                 transaction.onclick = ()=>{transactionDetailsComp.display(merchant.transactions[i])};
                 transactionsList.appendChild(transaction);
 
+                let totalRecipes = 0;
+                let totalPrice = 0;
+
+                for(let j = 0; j < merchant.transactions[i].recipes.length; j++){
+                    totalRecipes += merchant.transactions[i].recipes[j].quantity;
+                    totalPrice += merchant.transactions[i].recipes[j].recipe.price * merchant.transactions[i].recipes[j].quantity;
+                }
+
                 transaction.children[0].innerText = `${merchant.transactions[i].date.toLocaleDateString()} ${merchant.transactions[i].date.toLocaleTimeString()}`;
-                transaction.children[1].innerText = `${merchant.transactions[i].recipes.length} recipes sold`;
+                transaction.children[1].innerText = `${totalRecipes} recipes sold`;
+                transaction.children[2].innerText = `$${(totalPrice / 100).toFixed(2)}`;
             }
 
             this.isPopulated = true;
