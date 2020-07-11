ec5df219f838999f6eac83786c8e94d859a9e2de
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-07-11 09:23:34 +0300

    Fix all bugs on information page

--- views/informationPage/help.js
+++ views/informationPage/help.js
@@ -1,6 +1,10 @@
 window.helpObj = {
     display: function(){
-        clearScreen();
+        document.querySelector("#legalStrand").style.display = "none";
         document.querySelector("#helpStrand").style.display = "flex";
+
+        let button = document.getElementById("logInButton");
+        button.innerText="LEGAL";
+        button.onclick = ()=>{legalObj.display()};
     }
 }
--- views/informationPage/legal.js
+++ views/informationPage/legal.js
@@ -1,6 +1,13 @@
 window.legalObj = {
     display: function(){
-        clearScreen();
         document.querySelector("#legalStrand").style.display = "flex";
+        document.querySelector("#helpStrand").style.display = "none";
+
+        document.getElementById("joinButton").style.display = "none";
+        let button = document.getElementById("logInButton");
+        button.innerText="HELP";
+        button.onclick = ()=>{helpObj.display()};
     }
 }
+
+legalObj.display();
