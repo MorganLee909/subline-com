e05341f590352c21c5589b67319b6edf3bd3a143
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-10-27 14:22:57 +0300

    Fix bug with new transactions not being properly added in frontend.

--- views/dashboardPage/bundle.js
+++ views/dashboardPage/bundle.js
@@ -437,7 +437,7 @@ class Merchant{
         for(let i = 0; i < transaction.recipes.length; i++){
             const recipe = transaction.recipes[i];
             for(let j = 0; j < recipe.recipe.ingredients.length; j++){
-                const ingredient = recipe.recipe.ingredients[i];
+                const ingredient = recipe.recipe.ingredients[j];
                 if(ingredients[ingredient.ingredient.id]){
                     ingredients[ingredient.ingredient.id] += recipe.quantity * ingredient.quantity;
                 }else{
