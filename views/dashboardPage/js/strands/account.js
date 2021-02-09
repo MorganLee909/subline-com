37dc49d6edc034d52f106c5946a34fffc2fe4bdf
Lee Morgan <Morgan.LeeR@protonmail.com>
2021-02-09 17:25:36 +0300

    Add some very basic functionality to the account strand.

--- views/dashboardPage/js/strands/account.js
+++ views/dashboardPage/js/strands/account.js
@@ -1,6 +1,23 @@
 let account = {
     display: function(){
-        console.log("account page");
+        document.getElementById("accountStrandTitle").innerText = merchant.name;
+        document.getElementById("accountShowPassword").onclick = ()=>{this.togglePassword()};
+    },
+
+    togglePassword: function(){
+        let passBox = document.getElementById("changePasswordBox");
+        let button = document.getElementById("accountShowPassword");
+
+        if(passBox.style.display === "none"){
+            passBox.style.display = "flex";
+            button.innerText = "HIDE PASSWORD";
+        }else{
+            passBox.style.display = "none";
+            button.innerText = "CHANGE PASSWORD";
+            document.getElementById("accountCurrentPassword").value = "";
+            document.getElementById("accountNewPassword").value = "";
+            document.getElementById("accountConfirmPassword").value = "";
+        }
     }
 }
 
