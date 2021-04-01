826a54cc792bf9620a60e0b8360d4a7ad82a116d
Lee Morgan <Morgan.LeeR@protonmail.com>
2021-04-01 15:53:49 +0300

    Change verifyEmail to be basically plain text.

--- emails/verifyEmail.js
+++ emails/verifyEmail.js
@@ -1,17 +1,13 @@
 module.exports = (data)=>{
     return `
         <div id="verifyEmail">
-            <header style="width:100%;height:75px;background:rgb(0,27,45);">
-                <img src="https://i.postimg.cc/dQky3vPX/logo.png" alt="Subline Logo" style="height:50px;padding:12px 12px;float:left;">
-
-                <h3 style="color:rgb(255,99,107);font-size:30px;margin:15px 0 0 0;float:left;">THE SUBLINE</h3>
-            </header>
-
-            <h1 style="text-align:center;">Email Verification for ${data.name}</h1>
+            <p>This email is to verify the account for ${data.name} on The Subline.</p>
             
-            <p>Use the following link to verify your email:</p>
+            <p>Please use the following link to verify your email:</p>
+            <a href="${data.link}">${data.link}</a>
 
-            <p>${data.link}</p>
+            <p>Thank you for using The Subline</p>
+            <p>-Lee Morgan</p>
         </div>
     `;
 }
