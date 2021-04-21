3762da397837ac1d1333350918140ba116ad7a2d
Lee Morgan <me@leemorgan.io>
2021-04-21 14:11:49 +0300

    Remove mobile code from sidebars.css. Remove sidebars.css.

--- app.js
+++ app.js
@@ -52,10 +52,6 @@ require("./routes")(app);
 
 console.time("time");
 cssmerger([
-    // "./views/shared/shared.css",
-    // "./views/dashboardPage/dashboard.css",
-    "./views/dashboardPage/sidebars.css",
-
     "./views/shared/css/general.css",
     "./views/shared/css/loader.css",
     "./views/dashboardPage/css"
