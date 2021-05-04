48abda1a17964fd0488a12cb57eaeae547709f2d
Lee Morgan <me@leemorgan.io>
2021-05-04 10:58:26 +0300

    Recreating new recipe sidebar. Ingredients can be added/removed to the recipe.

--- views/dashboardPage/js/sidebars/newRecipe.js
+++ views/dashboardPage/js/sidebars/newRecipe.js
@@ -1,25 +1,48 @@
 let newRecipe = {
-    unselected: [],
-    selected: [],
+    unchosen: [],
 
     display: function(){
         document.getElementById("sidebarDiv").classList.add("sidebarWide");
 
-        let list = document.getElementById("recipeChoicesList");
-        for(let i = 0; i < merchant.recipes.length; i++){
-            this.unselected.push(merchant.recipes[i]);
+        for(let i = 0; i < merchant.ingredients.length; i++){
+            this.unchosen.push(merchant.ingredients[i].ingredient);
+        }
 
-            let recipe = document.createElement("button");
-            recipe.innerText = merchant.recipes[i];
-            recipe.classList.add("choosable");
-            recipe.onclick = ()=>{this.add(merchant.recipes[i], recipe)};
-            list.appendChild(recipe);
+        this.populateChoices();
+    },
+
+    populateChoices: function(){
+        this.unchosen.sort((a, b) => (a.name > b.name) ? 1 : -1);
+
+        let list = document.getElementById("recipeChoicesList");
+        while(list.children.length > 0){
+            list.removeChild(list.firstChild);
+        }
+
+        for(let i = 0; i < this.unchosen.length; i++){
+            let ingredient = document.createElement("button");
+            ingredient.innerText = this.unchosen[i].name;
+            ingredient.classList.add("choosable");
+            ingredient.onclick = ()=>{
+                this.add(this.unchosen[i]);
+                this.unchosen.splice(i, 1);
+                this.populateChoices();
+            };
+            list.appendChild(ingredient);
         }
     },
 
-    add: function(recipe, element){
-        console.log("adding");
-    }
+    add: function(ingredient){
+        let element = document.getElementById("newRecipeChosenIngredient").content.children[0].cloneNode(true);
+        element.children[0].children[0].innerText = ingredient.name;
+        element.children[0].children[1].onclick = ()=>{
+            this.unchosen.push(ingredient);
+            element.parentElement.removeChild(element);
+            this.populateChoices();
+        };
+        element.children[1].placeholder = `UNIT (${ingredient.unit.toUpperCase()})`;
+        document.getElementById("newRecipeChosenList").appendChild(element);
+    },
 };
 
 module.exports = newRecipe;
